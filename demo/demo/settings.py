381b9ceb0d2bf4649289a7e704a3854d2b6a523c
Kaxramonovravshan <r17ravshan@gmail.com>
2022-03-19 16:07:30 +0500

    ok

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +133,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 #         on_delete=models.CASCADE,
 #     )
 
-STATIC_ROOT = "/home/Qahramonov/hotel/demo/app/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
