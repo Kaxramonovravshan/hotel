cbc37836eca13e89f88fcdb58bef5951d9429510
Kaxramonovravshan <r17ravshan@gmail.com>
2022-05-17 20:03:30 +0500

    add

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^s%w7@ejf5++f(x=j1h6r1^u*n)#k455&ju))rbxjdfay945ge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','iwi.pythonanywhere.com']
 
 
 # Application definition
